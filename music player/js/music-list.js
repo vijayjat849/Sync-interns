f56a5909f2f73779743e28388dfbc3ab3da38a1e
vijayjat849 <118647362+vijayjat849@users.noreply.github.com>
2023-07-28 23:02:05 +0530

    Add files via upload

---
+++ music player/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Hancock",
+    artist: "Dino James",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Niddar",
+    artist: "Dino James",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Unstoppable",
+    artist: "Dino James",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Wishlist",
+    artist: "Dino James",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Dooriyan",
+    artist: "Dino James ft. Kaprila",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+];
